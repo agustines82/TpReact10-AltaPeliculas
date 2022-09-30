e6b8825f668fc98167b4554d4d73795305fc29ba
Agustin Sampayo <agustines82@gmail.com>
2022-09-30 20:14:05 -0300

    arreglo condicion en la formula de validacion de sinopsis

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -51,7 +51,7 @@ const Formulario = () => {
         }
     };
     const validarSinopsis = (sinopsis) => {
-        if (sinopsis.length >= 15 && sinopsis.length >= 80) {
+        if (sinopsis.length >= 15 && sinopsis.length <= 80) {
             setDisplaySinopsis("none");
             return true;
         } else {
